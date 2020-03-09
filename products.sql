f1b37037a80c9f70591191a42ae7d6a4353c1684
marija-nadj <61732850+marija-nadj@users.noreply.github.com>
2020-03-09 21:54:01 +0100

    Add files via upload

---
+++ products.sql
@@ -0,0 +1,8 @@
+SELECT * FROM sys.products;
+
+INSERT INTO products VALUES(1,'DVD player','55');
+INSERT INTO products VALUES(2,'toster','23');
+INSERT INTO products VALUES(3,'radio','15');
+INSERT INTO products VALUES(4,'TV','200');
+INSERT INTO products VALUES(5,'laptop','500');
+INSERT INTO products VALUES(6,'telephone','400');
